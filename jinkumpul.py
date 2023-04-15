ed5fae1753cdd8eaae6b4e7d6bc2ce44696c0929
Aurasindu <113558425+Aurasindu@users.noreply.github.com>
2023-04-15 20:11:08 +0700

    Update jinkumpul.py

--- jinkumpul.py
+++ jinkumpul.py
@@ -6,14 +6,14 @@ import load
 import random
 
 def run():
-    if __name__ == "__main__": 
+    
     # cek udh login blm
     # cek jin bukan
-        load.bahan_bangunan() 
-        load.data_bahan_bangunan[0] += random.randrange(1, 5)
-        load.data_bahan_bangunan[1] += random.randrange(1, 5)
-        load.data_bahan_bangunan[2] += random.randrange(1, 5)
-        print("Jin menemukan", load.data_bahan_bangunan[0], "pasir, ", load.data_bahan_bangunan[1], "batu, dan", load.data_bahan_bangunan[2], "air")
+    load.bahan_bangunan() 
+    load.data_bahan_bangunan[0] += random.randrange(1, 5)
+    load.data_bahan_bangunan[1] += random.randrange(1, 5)
+    load.data_bahan_bangunan[2] += random.randrange(1, 5)
+    print("Jin menemukan", load.data_bahan_bangunan[0], "pasir, ", load.data_bahan_bangunan[1], "batu, dan", load.data_bahan_bangunan[2], "air")
 
 
 
