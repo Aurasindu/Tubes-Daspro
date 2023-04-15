623ae7543bd189dde6c4ef0a60946f121f0dccaf
Aurasindu <113558425+Aurasindu@users.noreply.github.com>
2023-04-15 20:02:27 +0700

    Add files via upload

---
+++ jinkumpul.py
@@ -0,0 +1,18 @@
+import load
+import random
+jin_login = 'aura'
+
+if __name__ == "__main__": 
+    # cek udh login blm
+    # cek jin bukan
+    load.bahan_bangunan() 
+    load.data_bahan_bangunan[0] += random.randrange(1, 5)
+    load.data_bahan_bangunan[1] += random.randrange(1, 5)
+    load.data_bahan_bangunan[2] += random.randrange(1, 5)
+    print("Jin menemukan", load.data_bahan_bangunan[0], "pasir, ", load.data_bahan_bangunan[1], "batu, dan", load.data_bahan_bangunan[2], "air")
+
+
+
+
+
+
