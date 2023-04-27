e439b60fe803b1d996bd63bc40251ef8873f55c1
Aurasindu <aurasindu@gmail.com>
2023-04-27 08:26:54 +0700

    batch bgn

--- jinbangun.py
+++ jinbangun.py
@@ -1,38 +1,38 @@
-import load
 import random
 
 
 def run():
-    load.candi() 
-    load.bahan_bangunan() 
-    jumlah_candi = load.jumlah_candi
     pasir = random.randrange(1, 5)
     batu = random.randrange(1, 5)
     air = random.randrange(1, 5)
-    if pasir < load.data_bahan_bangunan[0]:
+    if pasir < data_bahan_bangunan[0]:
         print("Bahan bangunan tidak mencukupi.")
         print("Candi tidak bisa dibangun!")
-    elif batu < load.data_bahan_bangunan[1]:
+    elif batu < data_bahan_bangunan[1]:
         print("Bahan bangunan tidak mencukupi.")
         print("Candi tidak bisa dibangun!")
-    elif air < load.data_bahan_bangunan[2]:
+    elif air < data_bahan_bangunan[2]:
         print("Bahan bangunan tidak mencukupi.")
         print("Candi tidak bisa dibangun!")
-    elif load.jumlah_candi == 100:
+    elif jumlah_candi == 100:
         print("Candi berhasil dibangun.")
         print("Sisa candi yang perlu dibangun: 0")
     else:
         print("Candi berhasil dibangun.")
         print("Sisa candi yang perlu dibangun", 100-jumlah_candi)
-        load.data_bahan_bangunan[0] -= pasir
-        load.data_bahan_bangunan[1] -= batu
-        load.data_bahan_bangunan[2] -= air
+        data_bahan_bangunan[0] -= pasir
+        data_bahan_bangunan[1] -= batu
+        data_bahan_bangunan[2] -= air
     
     candi[jumlah_candi][0] = jumlah_candi + 1
     candi[jumlah_candi][1] = usernamejin
     candi[jumlah_candi][2] = pasir
     candi[jumlah_candi][3] = batu
     candi[jumlah_candi][4] = air
+
+
+
+
 
 
 
--- jinkumpul.py
+++ jinkumpul.py
@@ -1,21 +1,10 @@
-import load
-import random
-
-
-import load
 import random
 
 def run():
-    
     # cek udh login blm
     # cek jin bukan
-    load.bahan_bangunan() 
-    load.data_bahan_bangunan[0] += random.randrange(1, 5)
-    load.data_bahan_bangunan[1] += random.randrange(1, 5)
-    load.data_bahan_bangunan[2] += random.randrange(1, 5)
-    print("Jin menemukan", load.data_bahan_bangunan[0], "pasir, ", load.data_bahan_bangunan[1], "batu, dan", load.data_bahan_bangunan[2], "air")
-
-
-
-
-
+    bahan_bangunan() 
+    data_bahan_bangunan[0] += random.randrange(1, 5)
+    data_bahan_bangunan[1] += random.randrange(1, 5)
+    data_bahan_bangunan[2] += random.randrange(1, 5)
+    print("Jin menemukan", data_bahan_bangunan[0], "pasir, ", data_bahan_bangunan[1], "batu, dan", data_bahan_bangunan[2], "air")
